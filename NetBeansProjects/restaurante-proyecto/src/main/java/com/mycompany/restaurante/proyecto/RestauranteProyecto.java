39cab45cbce7761766803cbd3c218dcaf09e9e44
jose1 <jose1@Signus>
2023-07-04 19:01:18 -0600

    Primer commit

---
+++ NetBeansProjects/restaurante-proyecto/src/main/java/com/mycompany/restaurante/proyecto/RestauranteProyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restaurante.proyecto;
+
+/**
+ *
+ * @author jose1
+ */
+public class RestauranteProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
